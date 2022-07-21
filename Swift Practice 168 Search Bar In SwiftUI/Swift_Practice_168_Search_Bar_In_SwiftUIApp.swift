1bc50d66bcba6f4dde8f231eef0349019870c000
Dogpa <dogpa0130@gmail.com>
2022-07-21 12:44:23 +0800

    Initial Commit

---
+++ Swift Practice 168 Search Bar In SwiftUI/Swift_Practice_168_Search_Bar_In_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_168_Search_Bar_In_SwiftUIApp.swift
+//  Swift Practice 168 Search Bar In SwiftUI
+//
+//  Created by Dogpa's MBAir M1 on 2022/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_168_Search_Bar_In_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
